39db3b611d65cc02945f0c222fe1619f7cfc3b6f
ADITYA BASANTIA <adityabasantia729@gmail.com>
2025-06-08 21:35:09 +0530

    apiUpdated

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
-
 const Error = () => (
-  <div className="w-full flex justify-center items-center">
-    <h1 className="font-bold text-2xl text-white">Something went wrong. Please try again</h1>
+  <div className="w-full flex flex-col justify-center items-center">
+    <h1 className="font-bold text-2xl text-white">
+      API link is updated by the API provider
+    </h1>
+    <br />
+    <h1 className="font-bold text-2xl text-white">
+      Try to fetch data from new API link
+    </h1>
   </div>
 );
 
